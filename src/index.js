8cd600aac4936c26d07eec0478a29b406c209bda
Laysa Cunha <laysacunha@gmail.com>
2022-06-15 11:36:16 -0300

    route create users

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+  const user = users.find((user) => user.username === username);
+  if(!user){
+    return response.status(400).json({error: "User doens't exists!"});
+  }
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  if(!users.find((user) => user.username === username)){
+    const newUser = {
+      id: uuidv4,
+      name,
+      username,
+      todos: []
+    };
+    users.push(newUser);
+    return response.status(201).send(newUser);
+  };
+  return response.status(400).json({error: "User already exists!"});
+  
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
